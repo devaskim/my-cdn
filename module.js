12e8f298ee84676121abd13d898ac56c0d711301
devaskim <86909129+devaskim@users.noreply.github.com>
2022-06-06 22:30:09 +0500

    Update module.js

--- module.js
+++ module.js
@@ -1,6 +1,11 @@
-@NgModule({
-    declarations: []
-})
-export class HomeModule {
-
-}
+var AppModuleNgFactory = i0.ɵcmf(i1.AppModule, [i2.AppComponent], function(_l) {
+    return i0.ɵmod([i0.ɵmpd(512, i0.ComponentFactoryResolver, i0.ɵCodegenComponentFactoryResolver, [
+        [8, [i3.AppComponentNgFactory]],
+        [3, i0.ComponentFactoryResolver], i0.NgModuleRef
+    ]), i0.ɵmpd(5120, i0.LOCALE_ID, i0.ɵm, [
+        [3, i0.LOCALE_ID]
+    ]), ...
+    i0.ɵmpd(131584, i0.ApplicationRef, i0.ApplicationRef, [i0.NgZone, i0.ɵConsole, i0.Injector, i0.ErrorHandler, i0.ComponentFactoryResolver, i0.ApplicationInitStatus]), i0.ɵmpd(512, i0.ApplicationModule, i0.ApplicationModule, [i0.ApplicationRef]), i0.ɵmpd(512, i5.BrowserModule, i5.BrowserModule, [
+        [3, i5.BrowserModule]
+    ]), i0.ɵmpd(512, i6.ɵba, i6.ɵba, []), i0.ɵmpd(512, i6.FormsModule, i6.FormsModule, []), i0.ɵmpd(512, i1.AppModule, i1.AppModule, [])]);
+});
