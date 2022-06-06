06be849749002303d0b0f2e235b8a1e3c5777e23
devaskim <86909129+devaskim@users.noreply.github.com>
2022-06-06 22:25:15 +0500

    Update module.js

--- module.js
+++ module.js
@@ -1,16 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
- AppComponent
-  ],
-  imports: [
- BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: []
 })
-export class AppModule { }
+export class HomeModule {
+
+}
